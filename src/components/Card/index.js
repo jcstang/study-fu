72f53bb41620e06b5ff69aad66ae0ca7c2ae5e51
Jacob Stanger <jacobcstanger@gmail.com>
2020-07-07 23:28:41 -0600

    deep clean of comments

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -2,74 +2,22 @@ import React, { useState } from "react";
 import "./index.css";
 import { getNewBatchOfNumbersObject } from "../../utils/helperFunctions";
 
-// const topNumber = random();
-// const bottomNumber = random();
-// const multiplyResult = topNumber * bottomNumber;
-// // const initState = createChoiceArray(multiplyResult);
-// // const initState = freshUniqueArray(6, multiplyResult);
-// const initState = createChoicesArray(6, multiplyResult);
-// console.log(`Array of choices: ${initState}`);
-// console.log(`Answer: ${multiplyResult}`);
-
-// ****
-// const gameNumbers = getNewBatchOfNumbersObject();
-
-// const onClickHandler = (event) => {
-//   const chosenValue = event.target.id;
-//   // console.log(`chosenValue: ${chosenValue}`);
-//   // console.log(typeof chosenValue);
-//   // console.log("im clicked!");
-
-//   console.log(`chosenValue: ${chosenValue} answer: ${gameNumbers.answer}`);
-//   // console.log(typeof gameNumbers.answer);
-
-//   if (parseInt(chosenValue) === gameNumbers.answer) {
-//     console.log("correct!");
-//     setUserMessage();
-//     // TODO: what needs to happen when user chooses correct answer
-//   } else {
-//     console.log("wrong");
-//     // TODO: wrong answer, what now? reset game?
-//   }
-// };
-// const gameNumbers = getNewBatchOfNumbersObject();
-// const gameNumbers = {};
-
 const initValue = getNewBatchOfNumbersObject();
 
 export default function Card(props) {
   const [gameValues, setGameValues] = useState(initValue);
-
-  // console.log("props thing below");
-  // console.log(props.gameNumbers);
   const [userMessage, setUserMessage] = useState("");
-  // const [topNumber, setTopNumber] = useState(gameValues.topNumber);
-  // const [bottomNumber, setBottomNumber] = useState(
-  //   props.gameNumbers.bottomNumber
-  // );
-  // const [answer, setAnswer] = useState(props.gameNumbers.answer);
-  // const [choicesArray, setChoicesArray] = useState(props.gameNumbers.choices);
-  // const [topRowArray, setTopRowArray] = useState(props.gameNumbers.rowTop);
-  // const [bottomRowArray, setBottomArray] = useState(props.gameNumbers.rowBtm);
 
   const resetGame = () => {
     console.log("hello!, resetgame has been clicked");
     const newGameValues = getNewBatchOfNumbersObject();
     setGameValues(newGameValues);
     setUserMessage("");
-
-    // setTopNumber(newGameValues.topNumber);
-    // setBottomNumber(newGameValues.bottomNumber);
   };
 
   const onClickHandler = (event) => {
     const chosenValue = event.target.id;
-    // console.log(`chosenValue: ${chosenValue}`);
-    // console.log(typeof chosenValue);
-    // console.log("im clicked!");
-
     console.log(`chosenValue: ${chosenValue} answer: ${gameValues.answer}`);
-    // console.log(typeof gameNumbers.answer);
 
     if (parseInt(chosenValue) === gameValues.answer) {
       console.log("correct!");
